ea55ca22ddedd70477e36772de3cca176eca1d0f
Ed Lui <rewq142857@hotmail.com>
2019-12-30 16:58:48 +0800

    Update pyplot-hist.py

--- pyplot-hist.py
+++ pyplot-hist.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 name="informal_sent_len"
+#single col of values
 data=pd.read_csv(name+".txt")
 data.plot(kind='hist',range=[0,40],bins=20)
 plt.xlabel('Sentence Length')
